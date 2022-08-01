6f64771150aec005450b8abfbb09503f5a63dcab
shafiqahmad081 <shafiqahmad081@gmail.com>
2022-08-01 12:29:48 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:true,
+//   items:1,
+//   autoplay:true,
+//   dots:false,
+// })
+// $('.carousel').carousel({
+//   loop:false,
+//   nav:false,
+//   items:1,
+//   autoplay:false,
+//   dots:true,
+// })
+
+
+  $(document).ready(function($){
+    $(".owl-carousel").owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      items:1,
+      autoplay:true,
+      dots:true,
+    });
+  });
+
+
